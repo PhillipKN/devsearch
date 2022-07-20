0583907c8f0f67c05969836a50db09281273bfdf
Phillip Heita <phillipheita@gmail.com>
2022-07-20 20:33:46 +0200

    Initial Commit

---
+++ projects/views.py
@@ -0,0 +1,24 @@
+from multiprocessing import context
+import re
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
+
+
+def projects(request):
+    projects = Project.objects.all()
+
+    context = {'projects':projects}
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    print('projectObj;',projectObj)
+    return render(request, 'projects/single-projects.html', {'project':projectObj})
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form':form}
+    return render(request, "projects/project_form.html", context)
