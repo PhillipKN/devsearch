a18b7fe7523dade059d5d91790ffff43b5b724a3
Phillip Heita <phillipheita@gmail.com>
2022-07-21 17:33:13 +0200

    Create Project Added

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,4 @@
-from multiprocessing import context
-import re
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
@@ -20,5 +18,12 @@ def project(request, pk):
 
 def createProject(request):
     form = ProjectForm()
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+
     context = {'form':form}
     return render(request, "projects/project_form.html", context)
