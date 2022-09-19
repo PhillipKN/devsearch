d9d1eedaf5291a7e8ddbde1cebc1ee24e377f2bf
Phillip Heita <phillipheita@gmail.com>
2022-09-19 08:09:26 +0200

    User Model Added

--- users/models.py
+++ users/models.py
@@ -1,1 +1,14 @@
+from contextlib import nullcontext
+from hashlib import blake2b
 from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=500, blank=True, null=True)
+    short_intro = models.CharField(max_length=200, null=True, blank=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(null=True, blank = True, upload_to ='profiles/')
