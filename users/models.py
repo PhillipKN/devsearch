9ba9c4bd894c08b3e49b19d14774068c50793fe8
Phillip Heita <phillipheita@gmail.com>
2022-09-19 08:20:42 +0200

    User Model Migrations

--- users/models.py
+++ users/models.py
@@ -1,7 +1,9 @@
 from contextlib import nullcontext
+from email.policy import default
 from hashlib import blake2b
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 
 
@@ -11,4 +13,16 @@ class Profile(models.Model):
     email = models.EmailField(max_length=500, blank=True, null=True)
     short_intro = models.CharField(max_length=200, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
-    profile_image = models.ImageField(null=True, blank = True, upload_to ='profiles/')
+    profile_image = models.ImageField(null=True, blank = True, upload_to ='profiles/', default="profiles/user-default.png")
+    social_github = models.CharField(max_length=200, blank=True, null=True)
+    social_twitter = models.CharField(max_length=200, blank=True, null=True)
+    social_linkedin = models.CharField(max_length=200, blank=True, null=True)
+    social_youtube = models.CharField(max_length=200, blank=True, null=True)
+    social_website = models.CharField(max_length=200, blank=True, null=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.user.username)
+
+
+
