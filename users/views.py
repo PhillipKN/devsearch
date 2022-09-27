541042c654b832c151f5e3dc4688be6310a75c13
Phillip Heita <phillipheita@gmail.com>
2022-09-27 09:12:22 +0200

    Added Template

--- users/views.py
+++ users/views.py
@@ -9,3 +9,10 @@ def profiles(request):
     context = {'profiles':profiles}
 
     return render(request,'users/profiles.html', context)
+
+
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    context = {'profile':profile}
+    return render(request, 'users/user-profile.html', context)
