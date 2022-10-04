1b5cc9a9d7f8ca255a6c300defb63e86b994112d
Phillip Heita <phillipheita@gmail.com>
2022-10-04 09:10:56 +0200

    User Profile Added

--- users/views.py
+++ users/views.py
@@ -14,5 +14,12 @@ def profiles(request):
 
 def userProfile(request, pk):
     profile = Profile.objects.get(id=pk)
-    context = {'profile':profile}
+    
+    # Get all skills with descriptions
+    topSkills = profile.skill_set.exclude(description = "")
+    # Get skills with no description
+    otherSkills = profile.skill_set.filter(description = "")
+
+    context = {'profile':profile,'topSkills':topSkills,'otherSkills':otherSkills}
+
     return render(request, 'users/user-profile.html', context)
